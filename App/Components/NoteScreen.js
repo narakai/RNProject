15678924bc45845359c9a21e14767f0eac4e2a15
Leon Lai <narakai@126.com>
2016-04-27 17:49:48 +0800

    add scene transition

---
+++ App/Components/NoteScreen.js
@@ -0,0 +1,26 @@
+/**
+ * Created by leon on 16/4/27.
+ */
+import React, {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class NoteScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Create Note Screen!</Text>
+            </View>
+        );
+    }
+}
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
--- index.ios.js
+++ index.ios.js
@@ -4,49 +4,77 @@
  */
 'use strict';
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Navigator,
+    Text,
+    View
 } from 'react-native';
 
-class RNProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+import SimpleButton from './App/Components/SimpleButton.js';
+import NoteScreen from './App/Components/NoteScreen';
+
+class RNProject extends React.Component {
+    renderScene(route, navigator) {
+        switch (route.name) {
+            case 'home':
+                return (
+                    <View style={styles.container}>
+                        <SimpleButton
+                            onPress={() => {
+                            navigator.push({name: 'createNote'});
+                            }}
+                            customText='Create Note'
+                        />
+                    </View>
+                );
+            case 'createNote':
+                return (
+                    <NoteScreen />
+                );
+        }
+    }
+
+    render() {
+        return (
+            <Navigator initialRoute={{name: 'home'}} renderScene={this.renderScene}/>
+            //<View style={styles.container}>
+            //    <SimpleButton/>
+            //</View>
+            //  <View style={styles.container}>
+            //    <Text style={styles.welcome}>
+            //      Welcome to React Notes!
+            //    </Text>
+            //    <Text style={styles.instructions}>
+            //      To get started, edit index.ios.js
+            //    </Text>
+            //    <Text style={styles.instructions}>
+            //      Press Cmd+R to reload,{'\n'}
+            //      Cmd+D or shake for dev menu
+            //    </Text>
+            //  </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    }
+    //welcome: {
+    //  fontSize: 20,
+    //  textAlign: 'center',
+    //  margin: 10,
+    //},
+    //instructions: {
+    //  textAlign: 'center',
+    //  color: '#333333',
+    //  marginBottom: 5,
+    //},
 });
 
 AppRegistry.registerComponent('RNProject', () => RNProject);
